e791db26ede6f3a7cf012f7cbe4f4b5cfa6f3d59
FWZ <635344130@qq.com>
2016-10-07 09:38:31 +0800

    2

--- blog/NewsSpider/config.py
+++ blog/NewsSpider/config.py
@@ -2,24 +2,14 @@ from __future__ import absolute_import
 from datetime import timedelta
 from celery.schedules import crontab
 
-# CELERY_TASK_RESULT_EXPIRES=3600
-# CELERY_TASK_SERIALIZER='json'
-# CELERY_ACCEPT_CONTENT=['json']
-# CELERY_RESULT_SERIALIZER='json'
 
 CELERYBEAT_SCHEDULE = {
     'spider': {
         'task': 'main.spider',
-        'schedule': timedelta(minutes=30),
+        'schedule': timedelta(minutes=1),
         'args':(),
     },
 }
-#CELERYBEAT_SCHEDULE = {
-#    'add-every-2-seconds': {
-#        'task': 'proj.agent.add',
-#        'schedule': timedelta(seconds=3),
-#        'args': (16, 16)
-#    },
-#}
+
 
 CELERY_TIMEZONE = 'UTC'
